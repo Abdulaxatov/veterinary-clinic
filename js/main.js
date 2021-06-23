785209a8f795afe7b8c133158542fe4cf2c3d224
Abdulaxatov <samandarabdulaxatov55@gmail.com>
2021-06-23 22:26:05 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var elOpenModalButton = document.querySelector('.js-modal-open');
+var elModal = document.querySelector('.js-modal');
+var closeButton = document.querySelector('.exit-button');
+
+elOpenModalButton.addEventListener('click', function ()  {
+  elModal.classList.add('modal-open')
+});
+
+closeButton.addEventListener('click', function(){
+  elModal.classList.remove('modal-open')
+});
